94d808b0650fe4d1dbd6a47a9911abf928264a7b
Dean Reymen <deenrayman@outlook.com>
2023-05-28 19:57:02 +0300

    Form text input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-reval';
+  public title = 'angular-reval';
+
+  public constructor(
+    private readonly iconRegistry: MatIconRegistry,
+    private readonly domSanitizer: DomSanitizer
+  ) {
+    this.iconRegistry.addSvgIcon(
+      'mail',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('./assets/svg/mail.svg')
+    );
+    this.iconRegistry.addSvgIcon(
+      'question',
+      this.domSanitizer.bypassSecurityTrustResourceUrl(
+        './assets/svg/question.svg'
+      )
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InputComponent } from './input/input.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, InputComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatSelectModule,
     MatInputModule,
     MatFormFieldModule,
+    MatIconModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
