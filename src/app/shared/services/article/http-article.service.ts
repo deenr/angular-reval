0d427242eb5eea039023edc3dbf7aa1a857e8ed3
Dean Reymen <deenrayman@outlook.com>
2023-06-27 16:29:45 +0300

    Make use of firestore for news articles

--- src/app/shared/services/article/http-article.service.ts
+++ src/app/shared/services/article/http-article.service.ts
@@ -1,22 +1,49 @@
 import {Injectable} from '@angular/core';
-import {Observable, delay, of} from 'rxjs';
-import {articles} from './mock-articles';
+import {Observable, map} from 'rxjs';
 import {ArticleOverview} from '@shared/models/article/article-overview.model';
 import {Article} from '@shared/models/article/article.model';
+import {AngularFirestore, AngularFirestoreCollection, DocumentChangeAction} from '@angular/fire/compat/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpArticleService {
+  private newsRef: AngularFirestoreCollection<Article>;
+
+  public constructor(private readonly fireStore: AngularFirestore) {
+    this.newsRef = fireStore.collection<Article>('news');
+  }
+
   public getOverview(): Observable<ArticleOverview[]> {
-    return of(articles.map((articleJSON: any) => ArticleOverview.fromJSON(articleJSON))).pipe(delay(2000));
+    return this.newsRef.snapshotChanges().pipe(
+      map((changes: DocumentChangeAction<Article>[]) =>
+        changes.map((change: DocumentChangeAction<Article>) =>
+          ArticleOverview.fromJSON({
+            id: change.payload.doc.id,
+            ...change.payload.doc.data()
+          })
+        )
+      )
+    );
   }
 
   public getAll(): Observable<Article[]> {
-    return of(articles.map((articleJSON: any) => Article.fromJSON(articleJSON)));
+    return this.newsRef.snapshotChanges().pipe(
+      map((changes: DocumentChangeAction<Article>[]) =>
+        changes.map((change: DocumentChangeAction<Article>) =>
+          Article.fromJSON({
+            id: change.payload.doc.id,
+            ...change.payload.doc.data()
+          })
+        )
+      )
+    );
   }
 
   public getArticleById(id: string): Observable<Article> {
-    return of(Article.fromJSON(articles.find((articleJSON: any) => articleJSON.id === id))).pipe(delay(2000));
+    return this.newsRef
+      .doc(id)
+      .get()
+      .pipe(map((articleFirestoreJSON: any) => Article.fromJSON(articleFirestoreJSON.data())));
   }
 }
